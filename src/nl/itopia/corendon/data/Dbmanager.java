6594237fe24a32a210ee3ccc4cfd6e1314026f1a
fan1200 <Jeroentje@Jeroen-PC>
2014-11-15 23:11:28 +0100

    Added Database Manager plus working connection with database

---
+++ src/nl/itopia/corendon/data/Dbmanager.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.itopia.corendon.data;
+import java.sql.*;
+/**
+ *
+ * @author Jeroentje
+ */
+public class Dbmanager {
+    public static final String JDBC_EXCEPTION = "JDBC Exception: ";
+    public static final String SQL_EXCEPTION = "SQL Exception: ";
+    public Connection connection;
+    
+    private static final String dbHost = "sql4.freesqldatabase.com";
+    private static final String dbName = "sql458254";
+    private static final String dbUser = "sql458254";
+    private static final String dbPass = "kR5!eE6!";    
+    
+    /**
+     * Open database connection
+     */
+    public void openConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            /** Open connection */
+            connection = DriverManager.getConnection("jdbc:mysql://" + dbHost, dbUser, dbPass);
+            
+            /* set database */
+            connection.setSchema(dbName);
+        } catch (ClassNotFoundException e) {
+            System.err.println(JDBC_EXCEPTION + e);
+        } catch (java.sql.SQLException e) {
+            System.err.println(SQL_EXCEPTION + e);
+        }
+    }
+
+    /**
+     * Close database connection
+     */
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Executes a query without result.
+     * @param query, the SQl query
+     */
+    public void executeQuery(String query) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeQuery(query);
+        } catch (java.sql.SQLException e) {
+            System.err.println(SQL_EXCEPTION + e);
+        }
+    }
+
+    /**
+     * Executes a query with result.
+     * @param query, the SQL query
+     */
+    public ResultSet doQuery(String query) {
+        ResultSet result = null;
+        try {
+            Statement statement = connection.createStatement();
+            result = statement.executeQuery(query);
+        } catch (java.sql.SQLException e) {
+            System.err.println(SQL_EXCEPTION + e);
+        }
+        return result;
+    }
+    
+    /**
+     * Executes a query with result.
+     * @param query, the SQL query
+     */
+    public ResultSet insertQuery(String query) {
+        ResultSet result = null;
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            result = statement.getGeneratedKeys();
+        } catch (java.sql.SQLException e) {
+            System.err.println(SQL_EXCEPTION + e);
+        }
+        return result;
+    }    
+}
