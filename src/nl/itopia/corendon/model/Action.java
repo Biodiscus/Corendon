511f8c054a20e00acd7ab48ab18d639e79d3c213
NLerikNL <Erik-schouten@live.nl>
2014-11-18 11:50:43 +0100

    Action, LogAction

---
+++ src/nl/itopia/corendon/model/Action.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.itopia.corendon.model;
+
+/**
+ *
+ * @author Erik
+ */
+public class Action {
+    private int id;
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+    public int getID() {
+        return id;
+    }
+}
