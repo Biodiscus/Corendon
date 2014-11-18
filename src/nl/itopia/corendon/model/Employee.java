47ece088d74b3ac756cfc2dc3be283062688eb0a
NLerikNL <Erik-schouten@live.nl>
2014-11-18 10:47:26 +0100

    Customer, Employee, Role

---
+++ src/nl/itopia/corendon/model/Employee.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.itopia.corendon.model;
+
+/**
+ *
+ * @author Erik
+ */
+public class Employee {
+    int id;
+    String username;
+    String contactDetails;
+    String notes;
+    Role role;
+    Airport airport;
+    int createDate;
+    int lastOnline;
+    
+}
