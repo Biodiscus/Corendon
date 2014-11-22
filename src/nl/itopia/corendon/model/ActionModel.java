f898af91c5dec66f01d59ac1632bf41fae726438
Biodiscus <robin@biodiscus.net>
2014-11-22 17:14:37 +0100

    The action model didn't commit properly

--- src/nl/itopia/corendon/model/ActionModel.java
+++ src/nl/itopia/corendon/model/ActionModel.java
@@ -1,7 +1,38 @@
 package nl.itopia.corendon.model;
 
+import nl.itopia.corendon.data.Action;
+import nl.itopia.corendon.data.Airport;
+import nl.itopia.corendon.utils.Log;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * © 2014, Biodiscus.net Robin
  */
 public class ActionModel {
+    private static final ActionModel _default = new ActionModel();
+    private final DatabaseManager dbmanager = DatabaseManager.getDefault();
+
+    private ActionModel() {}
+
+    public Action getAction(int id) {
+        Action action = null;
+
+        try {
+            ResultSet result = dbmanager.doQuery("SELECT * FROM action WHERE id = "+ id);
+            if(result.next()) {
+                String name = result.getString("name");
+                action = new Action(id, name);
+            }
+        } catch (SQLException e) {
+            Log.display("SQLEXCEPTION", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        }
+
+        return action;
+    }
+
+    public static ActionModel getDefault() {
+        return _default;
+    }
 }
