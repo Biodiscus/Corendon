d541dc0dcd069fb494a8924a4348012048f1753d
NLerikNL <Erik-schouten@live.nl>
2014-11-23 13:41:09 +0100

    AdminController

--- src/nl/itopia/corendon/controller/AdministratorController.java
+++ src/nl/itopia/corendon/controller/AdministratorController.java
@@ -5,11 +5,17 @@
  */
 package nl.itopia.corendon.controller;
 
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import nl.itopia.corendon.data.Employee;
+//import nl.itopia.corendon.data.Luggage;
 import nl.itopia.corendon.model.DatabaseManager;
+import nl.itopia.corendon.model.EmployeeModel;
+//import nl.itopia.corendon.model.LuggageModel;
 import nl.itopia.corendon.mvc.Controller;
 import nl.itopia.corendon.mvc.View;
 import nl.itopia.corendon.utils.Log;
@@ -23,18 +29,25 @@ public class AdministratorController extends Controller {
     private Employee employee;
     private DatabaseManager dbManager;
     
-    @FXML public Button test_button;
-
-    public AdministratorController(int width, int height) {
-        registerFXML("gui/TestGUI.fxml");
+    @FXML private TableColumn <Employee,String>userIDtable;
+    @FXML private TableColumn <Employee,String>usernametable;
+    @FXML private TableColumn <Employee,String>userLoginIDtable;
+    
+    public AdministratorController() {
+        registerFXML("gui/Overview_administrator.fxml");
     }
+    void initializeTable(){
+        //Create columns and set their datatype
+        
+        userIDtable.setCellValueFactory(new PropertyValueFactory<Employee, String>("ID"));
+        usernametable.setCellValueFactory(new PropertyValueFactory<Employee, String>("Username"));
+        userLoginIDtable.setCellValueFactory(new PropertyValueFactory<Employee, String>("Login ID"));
+        
+        
+//        //MAAK LIST VAN LUGGAGE OBJECTS EN VUL DE KOLOMMEN
+//        LuggageModel luggageModel = LuggageModel.getDefault(); 
+//        List<Employee> employeeList = EmployeeModel.getAllEmployee();
+//        luggageInfo.getItems().setAll(luggageList);
 
-    public void handle(ActionEvent e) {
-        Log.display("Button clicked", e);
-    }
-
-    @Override
-    public View getView() {
-        return view;
     }
 }
