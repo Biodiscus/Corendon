69ce468b3fef1e5f6300ced638edf57b6dbd1243
Robin de Jong <drop@biodiscus.net>
2014-11-25 17:49:13 +0100

    DateModel, to use time in java with the timestamps from the database

--- src/nl/itopia/corendon/model/DateModel.java
+++ src/nl/itopia/corendon/model/DateModel.java
@@ -1,5 +1,10 @@
 package nl.itopia.corendon.model;
 
+import nl.itopia.corendon.utils.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * © 2014, Biodiscus.net Robin
  */
@@ -10,6 +15,28 @@ public class DateModel {
 
     public long getCurrentTimeStamp() {
         return System.currentTimeMillis() / 1000;
+    }
+
+    public String formatDate(String format, long timestamp) {
+        Date date = timestampToDate(timestamp);
+        SimpleDateFormat f = new SimpleDateFormat(format);
+        return f.format(date);
+    }
+
+    public Date timestampToDate(long stamp) {
+        return new Date(stamp * 1000);
+    }
+
+    public String getTimestampDate(long timestamp) {
+        String day = formatDate("E yyyy.MM.dd 'at' hh:mm:ss a zzz", timestamp);
+        return formatDate("yyyy.MM.dd", timestamp);
+    }
+
+    public int getTimestampDay(long timestamp) {
+        String day = formatDate("E yyyy.MM.dd 'at' hh:mm:ss a zzz", timestamp);
+//        Log.display(day, timestamp);
+
+        return 0;
     }
 
     public static DateModel getDefault() {
