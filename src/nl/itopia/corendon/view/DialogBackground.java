3be58a08422a5f918db79b9e037cf57ea8af9f39
Biodiscus <robin@biodiscus.net>
2014-11-25 18:21:40 +0100

    Git decided not to correctly update the DialogBackground

--- src/nl/itopia/corendon/view/DialogBackground.java
+++ src/nl/itopia/corendon/view/DialogBackground.java
@@ -1,7 +1,21 @@
 package nl.itopia.corendon.view;
 
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import nl.itopia.corendon.mvc.View;
+
 /**
  * © 2014, Biodiscus.net Robin
  */
-public class DialogBackground {
+public class DialogBackground extends View {
+    public DialogBackground(Scene scene) {
+        double width = scene.getWidth();
+        double height = scene.getHeight();
+
+        Color color = new Color(0, 0, 0, 0.5);
+
+        Rectangle rectangle = new Rectangle(width, height, color);
+        getChildren().add(rectangle);
+    }
 }
