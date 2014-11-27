b408022aee8dc081d0e156730cb51763bdf31aaf
Biodiscus <robin@biodiscus.net>
2014-11-27 23:42:10 +0100

    Renaming EditEmployeeController is giving errors due to the rename

--- src/nl/itopia/corendon/controller/administrator/EditEmployeeController.java
+++ src/nl/itopia/corendon/controller/administrator/EditEmployeeController.java
@@ -1,33 +1,7 @@
 package nl.itopia.corendon.controller.administrator;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import nl.itopia.corendon.data.Employee;
-import nl.itopia.corendon.model.EmployeeModel;
-import nl.itopia.corendon.mvc.Controller;
-
 /**
- *
- * @author wieskueter.com
+ * © 2014, Biodiscus.net Robin
  */
-public class EditEmployeeController extends Controller{
-    @FXML private Button addButton, cancelButton;
-
-    public EditEmployeeController(int userId) {
-        this(EmployeeModel.getDefault().getEmployee(userId));
-    }
-
-    public EditEmployeeController(Employee employee) {
-        // Set view
-        registerFXML("gui/add_user.fxml");
-
-        // TODO: Do something with the employee
-
-        cancelButton.setOnAction(this::cancelHandler);
-    }
-
-    private void cancelHandler(ActionEvent e) {
-        removeController(this);
-    }
+public class EditEmployeeController {
 }
