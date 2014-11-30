6b7f613be72b394c108d9b89a2b4db16e79231c9
Biodiscus <robin@biodiscus.net>
2014-11-30 14:13:48 +0100

    My last view commits didn't include the content of the EditUserController

--- src/nl/itopia/corendon/controller/administrator/EditUserController.java
+++ src/nl/itopia/corendon/controller/administrator/EditUserController.java
@@ -1,7 +1,31 @@
 package nl.itopia.corendon.controller.administrator;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import nl.itopia.corendon.data.Employee;
+import nl.itopia.corendon.model.EmployeeModel;
+import nl.itopia.corendon.mvc.Controller;
+
 /**
  * © 2014, Biodiscus.net Robin
  */
-public class EditUserController {
+public class EditUserController extends Controller {
+    @FXML
+    private Button addButton, cancelButton;
+
+    public EditUserController(int userId) {
+        this(EmployeeModel.getDefault().getEmployee(userId));
+    }
+
+    public EditUserController(Employee employee) {
+    // Set view
+        registerFXML("gui/add_user.fxml");
+        // TODO: Do something with the employee
+        cancelButton.setOnAction(this::cancelHandler);
+    }
+
+    private void cancelHandler(ActionEvent e) {
+        removeController(this);
+    }
 }
