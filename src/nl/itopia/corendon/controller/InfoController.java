4f86b3598c130e5e08777f1477924a61f34293cc
cxzzy <wies_kueter@hotmail.com>
2014-12-09 13:02:34 +0100

    Edit user role and airport.

--- src/nl/itopia/corendon/controller/InfoController.java
+++ src/nl/itopia/corendon/controller/InfoController.java
@@ -10,7 +10,7 @@ public class InfoController extends Controller {
     
     public InfoController()
     {
-        registerFXML("gui/info_screen.fxml");
+        registerFXML("gui/help_function.fxml");
         System.out.println("This is the info controller");
     }
 }
---
+++ src/nl/itopia/corendon/utils/dropdownLists.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.itopia.corendon.utils;
+
+/**
+ *
+ * @author Kueter
+ */
+public class dropdownLists {
+    
+    
+    public static void test()
+    {
+        System.out.println("Dropdownlist test.");
+    }
+}
